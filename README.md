86845eb96ecfa39b2d7b9c44180692a02913cea0
zhengzenghao <zzh@zhengzenghaodeMacBook-Air.local>
2025-08-09 20:06:36 +0800

    update READEME,demo,requirements.txt

--- README.md
+++ README.md
@@ -1,18 +1,36 @@
 # Spectral Compression Transformer with Line Pose Graph
 
+<a href="https://pytorch.org/get-started/locally/"><img alt="PyTorch" src="https://img.shields.io/badge/PyTorch-ee4c2c?logo=pytorch&logoColor=white"></a>
+
 ## Abstract
+
 Transformer-based 3D human pose estimation methods suffer from high computational costs due to the quadratic complexity of self-attention with respect to sequence length. Additionally, pose sequences often contain significant redundancy between frames. In this work, we introduce the Spectral Compression Transformer (SCT) to reduce sequence length and accelerate computation. The SCT encoder treats hidden features between blocks as Temporal Feature Signals (TFS) and applies the Discrete Cosine Transform, a Fourier transform-based technique, to determine the spectral components to be retained. By filtering out certain high-frequency noise components, SCT compresses the sequence length and reduces redundancy. To further enrich the input sequence with prior structural information, we propose the Line Pose Graph (LPG) based on line graph theory. The LPG generates skeletal position information that complements the input 2D joint positions, thereby improving the model's performance. Finally, we design a dual-stream network architecture to effectively model spatial joint relationships and the compressed motion trajectory within the pose sequence. Extensive experiments on two benchmark datasets demonstrate that our model achieves state-of-the-art performance with improved computational efficiency.
 
-## Installation
+## Highlights
+🚀 Improve inference speed using SCT
+
+✅ Enhance the model's expressive ability
+
+💡 Embed into other models and manually fine-tune sigma to improve efficiency and accuracy.
+<p align="center">
+  <img src="figure/pareto_frontier.png" alt="Pareto Frontier" width="60%">
+  <br>
+  <em>FPS vs. MPJPE in mainstream 3DHPE backbone</em>
+</p>
+
+
+## Environment
+For installation of the project dependencies, please run:
 ```bash
 conda create -n sct python=3.7 anaconda
 conda activate sct
 # Please install PyTorch according to your CUDA version.
 conda install pytorch torchvision torchaudio pytorch-cuda=11.6 -c pytorch -c nvidia
+pip install -r requirements.txt
 ```
 
 ## Data
-
+### Human3.6M
 1. Download the finetuned Stacked Hourglass detections and our preprocessed H3.6M data [here](https://1drv.ms/u/s!AvAdh0LSjEOlgU7BuUZcyafu8kzc?e=vobkjZ) and unzip it to `data/motion3d`.
 
 2. Slice the motion clips (len=243, stride=81)
@@ -22,23 +40,31 @@ conda install pytorch torchvision torchaudio pytorch-cuda=11.6 -c pytorch -c nvi
    ```
 
 ## Model Pretraining
-The pretrain weights of our model can be downloaded from [here](https://drive.google.com/file/d/179H4UzosuSNnvn6WQ3_Mh1gKcf0SK14T/view?usp=sharing). Place it in the checkpoint folder.
+### Human3.6M
+The pretrain weights of our model can be downloaded from [here](https://drive.google.com/file/d/179H4UzosuSNnvn6WQ3_Mh1gKcf0SK14T/view?usp=sharing). Place it in the 'checkpoint' folder.
 
 
-## Evaluate
-
+## Evaluation
+### Human3.6M
 ```bash
 python train.py --config configs/MB_train_h36m.yaml --evaluate checkpoint/best_epoch.bin        
 ```
+
+## Demo
+First, you need to enter the demo/lib/checkpoint folder, refer to the README file there to download the YOLOv3 and HRNet weights, and place them in the './demo/lib/checkpoint' folder.
+
+Then, execute the following command in the root directory of the file:
+```
+python demo/vis.py --video sample_video.mp4
+```
+Sample demo output:
+
+<p align="center">
+<img src='figure/dance.gif' width="100%" alt="no img" />
+</p>
 
 ## Acknowledgement
 Our code refers to the following repositories:
 - [MotionBERT](https://github.com/Walter0807/MotionBERT)
 
 We thank the authors for releasing their codes.
-
-
-
-
-
-
---
+++ demo/vis.py
@@ -0,0 +1,304 @@
+import sys
+import argparse
+import cv2
+from lib.preprocess import h36m_coco_format, revise_kpts
+from lib.hrnet.gen_kpts import gen_video_kpts as hrnet_pose
+import os
+import numpy as np
+import torch
+import torch.nn as nn
+import glob
+from tqdm import tqdm
+import copy
+
+sys.path.append(os.getcwd())
+from demo.lib.utils import normalize_screen_coordinates, camera_to_world
+from lib.model.SCTformer import SCTformer
+import matplotlib
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.gridspec as gridspec
+
+plt.switch_backend('agg')
+matplotlib.rcParams['pdf.fonttype'] = 42
+matplotlib.rcParams['ps.fonttype'] = 42
+
+
+def show2Dpose(kps, img):
+    connections = [[0, 1], [1, 2], [2, 3], [0, 4], [4, 5],
+                   [5, 6], [0, 7], [7, 8], [8, 9], [9, 10],
+                   [8, 11], [11, 12], [12, 13], [8, 14], [14, 15], [15, 16]]
+
+    LR = np.array([0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0], dtype=bool)
+
+    lcolor = (255, 0, 0)
+    rcolor = (0, 0, 255)
+    thickness = 3
+
+    for j, c in enumerate(connections):
+        start = map(int, kps[c[0]])
+        end = map(int, kps[c[1]])
+        start = list(start)
+        end = list(end)
+        cv2.line(img, (start[0], start[1]), (end[0], end[1]), lcolor if LR[j] else rcolor, thickness)
+        cv2.circle(img, (start[0], start[1]), thickness=-1, color=(0, 255, 0), radius=3)
+        cv2.circle(img, (end[0], end[1]), thickness=-1, color=(0, 255, 0), radius=3)
+
+    return img
+
+
+def show3Dpose(vals, ax):
+    ax.view_init(elev=15., azim=70)
+
+    # lcolor=(0,0,1)
+    # rcolor=(1,0,0)
+
+    I = np.array([0, 0, 1, 4, 2, 5, 0, 7, 8, 8, 14, 15, 11, 12, 8, 9])
+    J = np.array([1, 4, 2, 5, 3, 6, 7, 8, 14, 11, 15, 16, 12, 13, 9, 10])
+
+    LR = np.array([0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0], dtype=bool)
+
+    for i in np.arange(len(I)):
+        x, y, z = [np.array([vals[I[i], j], vals[J[i], j]]) for j in range(3)]
+        # ax.plot(x, y, z, lw=2, color = lcolor if LR[i] else rcolor)
+        ax.plot(x, y, z, lw=2)
+        ax.scatter(x, y, z)
+
+    # RADIUS = 0.72
+    # RADIUS_Z = 0.7
+
+    # xroot, yroot, zroot = vals[0,0], vals[0,1], vals[0,2]
+    # ax.set_xlim3d([-RADIUS+xroot, RADIUS+xroot])
+    # ax.set_ylim3d([-RADIUS+yroot, RADIUS+yroot])
+    # ax.set_zlim3d([-RADIUS_Z+zroot, RADIUS_Z+zroot])
+    # ax.set_aspect('auto') # works fine in matplotlib==2.2.2
+
+    RADIUS = 0.8
+
+    ax.set_xlim3d([-RADIUS, RADIUS])
+    ax.set_ylim3d([-RADIUS, RADIUS])
+    ax.set_aspect('auto')  # msj oringin is equal
+
+    white = (1.0, 1.0, 1.0, 0.0)
+    ax.xaxis.set_pane_color(white)
+    ax.yaxis.set_pane_color(white)
+    ax.zaxis.set_pane_color(white)
+
+    ax.tick_params('x', labelbottom=False)
+    ax.tick_params('y', labelleft=False)
+    ax.tick_params('z', labelleft=False)
+
+
+def get_pose2D(video_path, output_dir):
+    cap = cv2.VideoCapture(video_path)
+    width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+    height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+    print('\nGenerating 2D pose...')
+    keypoints, scores = hrnet_pose(video_path, det_dim=416, num_peroson=1, gen_output=True)
+    keypoints, scores, valid_frames = h36m_coco_format(keypoints, scores)
+
+    # Add conf score to the last dim
+    keypoints = np.concatenate((keypoints, scores[..., None]), axis=-1)
+
+    output_dir += 'input_2D/'
+    os.makedirs(output_dir, exist_ok=True)
+
+    output_npz = output_dir + 'keypoints.npz'
+    np.savez_compressed(output_npz, reconstruction=keypoints)
+
+
+def img2video(video_path, output_dir):
+    cap = cv2.VideoCapture(video_path)
+    fps = int(cap.get(cv2.CAP_PROP_FPS)) + 5
+
+    fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+
+    names = sorted(glob.glob(os.path.join(output_dir + 'pose/', '*.png')))
+    img = cv2.imread(names[0])
+    size = (img.shape[1], img.shape[0])
+
+    videoWrite = cv2.VideoWriter(output_dir + video_name + '.mp4', fourcc, fps, size)
+
+    for name in names:
+        img = cv2.imread(name)
+        videoWrite.write(img)
+
+    videoWrite.release()
+
+
+def showimage(ax, img):
+    ax.set_xticks([])
+    ax.set_yticks([])
+    plt.axis('off')
+    ax.imshow(img)
+
+
+def resample(n_frames):
+    even = np.linspace(0, n_frames, num=243, endpoint=False)
+    result = np.floor(even)
+    result = np.clip(result, a_min=0, a_max=n_frames - 1).astype(np.uint32)
+    return result
+
+
+def turn_into_clips(keypoints):
+    clips = []
+    n_frames = keypoints.shape[1]
+    if n_frames <= 243:
+        new_indices = resample(n_frames)
+        clips.append(keypoints[:, new_indices, ...])
+        downsample = np.unique(new_indices, return_index=True)[1]
+    else:
+        for start_idx in range(0, n_frames, 243):
+            keypoints_clip = keypoints[:, start_idx:start_idx + 243, ...]
+            clip_length = keypoints_clip.shape[1]
+            if clip_length != 243:
+                new_indices = resample(clip_length)
+                clips.append(keypoints_clip[:, new_indices, ...])
+                downsample = np.unique(new_indices, return_index=True)[1]
+            else:
+                clips.append(keypoints_clip)
+    return clips, downsample
+
+
+
+def get_pose3D(video_path, output_dir):
+    sct = SCTformer(dim_in=3, dim_out=3, dim_feat=512, dim_rep=512,
+                 depth=5, num_heads=8, mlp_ratio=2,
+                 num_joints=17, maxlen=243,
+                 qkv_bias=True, qk_scale=None, drop_rate=0., attn_drop_rate=0., drop_path_rate=0., norm_layer=nn.LayerNorm,
+                 att_fuse=True, dct_ratio=0.6, freq_prune_layer=[0,1,2,3,4])
+
+    if torch.cuda.is_available():
+        sct = nn.DataParallel(sct)
+    sct_chk_filename = "checkpoint/best_epoch.bin"
+    checkpoint = torch.load(sct_chk_filename, map_location=lambda storage, loc: storage)
+    sct.load_state_dict(checkpoint['model_pos'], strict=False)
+    sct.cuda()
+    sct.eval()
+
+    ## input
+    keypoints = np.load(output_dir + 'input_2D/keypoints.npz', allow_pickle=True)['reconstruction']
+
+    clips, downsample = turn_into_clips(keypoints)
+
+    cap = cv2.VideoCapture(video_path)
+    video_length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    ## 3D
+    print('\nGenerating 2D pose image...')
+    for i in tqdm(range(video_length)):
+        ret, img = cap.read()
+        if img is None:
+            continue
+        img_size = img.shape
+
+        input_2D = keypoints[0][i]
+
+        image = show2Dpose(input_2D, copy.deepcopy(img))
+
+        output_dir_2D = output_dir + 'pose2D/'
+        os.makedirs(output_dir_2D, exist_ok=True)
+        cv2.imwrite(output_dir_2D + str(('%04d' % i)) + '_2D.png', image)
+
+    joints_left = [4, 5, 6, 11, 12, 13]
+    joints_right = [1, 2, 3, 14, 15, 16]
+    print('\nGenerating 3D pose...')
+    for idx, clip in enumerate(clips):
+        input_2D = normalize_screen_coordinates(clip, w=img_size[1], h=img_size[0])
+
+        input_2D_aug = copy.deepcopy(input_2D)
+        input_2D_aug[..., 0] *= -1
+        input_2D_aug[..., joints_left + joints_right, :] = input_2D_aug[..., joints_right + joints_left, :]
+
+        input_2D = torch.from_numpy(input_2D.astype('float32')).cuda()
+        input_2D_aug = torch.from_numpy(input_2D_aug.astype('float32')).cuda()
+
+        output_3D = sct(input_2D)
+
+        if idx == len(clips) - 1:
+            output_3D = output_3D[:, downsample]
+
+        output_3D[:, :, 0, :] = 0
+        post_out_all = output_3D[0].cpu().detach().numpy()
+
+        for j, post_out in enumerate(post_out_all):
+            rot = [0.1407056450843811, -0.1500701755285263, -0.755240797996521, 0.6223280429840088]
+            rot = np.array(rot, dtype='float32')
+            post_out = camera_to_world(post_out, R=rot, t=0)
+
+            post_out[:, 2] -= np.min(post_out[:, 2])
+            max_value = np.max(post_out)
+            post_out /= max_value
+
+            fig = plt.figure(figsize=(9.6, 5.4))
+            gs = gridspec.GridSpec(1, 1)
+            gs.update(wspace=-0.00, hspace=0.05)
+            ax = plt.subplot(gs[0], projection='3d')
+            show3Dpose(post_out, ax)
+
+            output_dir_3D = output_dir + 'pose3D_ours/'
+            os.makedirs(output_dir_3D, exist_ok=True)
+            str(('%04d' % (idx * 243 + j)))
+            plt.savefig(output_dir_3D + str(('%04d' % (idx * 243 + j))) + '_3D.png', dpi=200, format='png',
+                        bbox_inches='tight')
+            plt.close(fig)
+
+    print('Generating 3D pose successful!')
+
+    ## all
+    image_2d_dir = sorted(glob.glob(os.path.join(output_dir_2D, '*.png')))
+    image_3d_dir = sorted(glob.glob(os.path.join(output_dir_3D, '*.png')))
+
+    print('\nGenerating demo...')
+    for i in tqdm(range(len(image_3d_gt_dir) - 1)):
+        image_2d = plt.imread(image_2d_dir[i])
+        image_3d = plt.imread(image_3d_dir[i])
+
+        ## crop
+        # edge = (image_2d.shape[1] - image_2d.shape[0]) // 2
+        edge = 150
+        image_2d = image_2d[:, edge:image_2d.shape[1] - edge]
+
+        edge = 130
+        # image_3d = image_3d[edge:image_3d.shape[0] - edge, edge:image_3d.shape[1] - edge]
+
+        ## show
+        font_size = 12
+        fig = plt.figure(figsize=(12.8, 5.4))  # fig = plt.figure(figsize=(9.6, 5.4))
+        ax = plt.subplot(121)
+        showimage(ax, image_2d)
+        ax.set_title("Input", fontsize=font_size)
+
+        ax = plt.subplot(122)
+        showimage(ax, image_3d)
+        ax.set_title("Reconstruction", fontsize=font_size)
+
+        ## save
+        output_dir_pose = output_dir + 'pose/'
+        os.makedirs(output_dir_pose, exist_ok=True)
+        plt.subplots_adjust(top=None, bottom=0.15, right=None, left=None, hspace=0, wspace=0)
+        plt.margins(0, 0)
+        plt.savefig(output_dir_pose + str(('%04d' % i)) + '_pose.png', dpi=200, bbox_inches='tight')
+        plt.close(fig)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--video', type=str, default='dance.mp4', help='input video')
+    parser.add_argument('--gpu', type=str, default='0', help='gpu')
+    args = parser.parse_args()
+
+    os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu
+
+    video_path = './demo/video/' + args.video
+    video_name = video_path.split('/')[-1].split('.')[0]
+    output_dir = './demo/output/' + video_name + '/'
+
+    get_pose2D(video_path, output_dir)
+
+    get_pose3D(video_path, output_dir)
+
+    img2video(video_path, output_dir)
+
+    print('Generating demo successful!')
